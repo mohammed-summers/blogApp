9558fc130f85c4df396b23ba2c0e113d2daa8bdd
Mohammed <mohammed.k.summers@gmail.com>
2023-02-19 11:01:34 -0600

    mks-10011: getCommentById Api

--- src/main/java/com/springboot/blog/repository/CommentRepository.java
+++ src/main/java/com/springboot/blog/repository/CommentRepository.java
@@ -1,11 +1,11 @@
 package com.springboot.blog.repository;
 
+import com.springboot.blog.dto.CommentDto;
 import com.springboot.blog.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Set;
 
 public interface CommentRepository extends JpaRepository <Comment, Long> {
-    List<Comment> findByPostId(Long postId);
+    List<Comment> findCommentByPostId(Long postId);
 }
