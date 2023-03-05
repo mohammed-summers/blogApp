91a478e93baefdf9d466e5f15832658154a323c2
Mohammed <mohammed.k.summers@gmail.com>
2023-03-05 08:57:29 -0600

    mks-1020: validating comment body request

--- src/main/java/com/springboot/blog/dto/CommentDto.java
+++ src/main/java/com/springboot/blog/dto/CommentDto.java
@@ -1,12 +1,23 @@
 package com.springboot.blog.dto;
 
 import com.springboot.blog.entity.Post;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(message = "Comment body must be a minimum 10 characters")
     private String body;
 }
